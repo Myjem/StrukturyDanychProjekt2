583e376f08a817cef5b3b2049adbbeb7fd9176e1
MANDRW <mandrzejewski132@gmail.com>
2024-04-15 23:02:49 +0200

    base class

---
+++ base.h
@@ -0,0 +1,14 @@
+#ifndef SD_PROJ2_BASE_H
+#define SD_PROJ2_BASE_H
+class Base{//base class for all data structures with virtual methods
+public:
+    virtual void insert(int e,int p)=0;//e-element, p-priority
+    virtual int extract_max()=0;
+    virtual int pick()=0;
+    virtual void modify_key(int e,int p);
+    virtual int get_size()=0;
+    virtual void print()=0;
+    virtual ~Base(){};
+};
+
+#endif
