ee86bc29e4be448b2941149fcebb5e4746320e80
MANDRW <mandrzejewski132@gmail.com>
2024-04-17 23:57:55 +0200

    heap queue start with implementation

--- base.h
+++ base.h
@@ -1,10 +1,11 @@
 #ifndef SD_PROJ2_BASE_H
 #define SD_PROJ2_BASE_H
+#include "queue_node.h"
 class Base{//base class for all data structures with virtual methods
 public:
     virtual void insert(int e,int p)=0;//e-element, p-priority
-    virtual int extract_max()=0;
-    virtual int peek()=0;
+    virtual node extract_max()=0;
+    virtual node find_max()=0;
     virtual void modify_key(int e,int p);
     virtual int get_size()=0;
     virtual void print()=0;
--- heap_queue.h
+++ heap_queue.h
@@ -2,4 +2,21 @@
 #define SD_PROJ2_HEAP_QUEUE_H
 #include"base.h"
 #include"queue_node.h"
+#include "heap.h"
+
+class Heap_queue::public base{
+private:
+    Heap heap;
+    int size=0;
+public:
+    Heap_queue();
+    void insert(int e,int p)=0;//e-element, p-priority
+    node extract_max()=0;
+    node find_max()=0;
+    void modify_key(int e,int p);
+    int get_size()=0;
+    void print()=0;
+    ~Heap_queue();
+};
+
 #endif
