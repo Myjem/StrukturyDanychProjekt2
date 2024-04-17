30138ad302d7a59a9bf3cc13aa57a6817a4b2d54
MANDRW <mandrzejewski132@gmail.com>
2024-04-17 23:33:55 +0200

    heap implementation

--- heap.h
+++ heap.h
@@ -1,23 +1,21 @@
 #ifndef SD_PROJ2_HEAP_H
 #define SD_PROJ2_HEAP_H
-#include"ArrayList.h"
+#include"array_list.h"
 #include"queue_node.h"
 
 class Heap{
 private:
-    ArrayList tab;
+    ArrayList<node>tab;
+    void heapify_up();
+    void heapify_down();
 public:
-    int parent(int i);//i-index
-    int left_child(int i);
-    int right_child(int i);
     void insert();
-    int extract_max();
-    int find_max();
-    int find_by_key(int p);//p-priority
-    int find_by_value(int v);//v-value
+    node extract_max();
+    node find_max();
+    node find_by_key(int p);//p-priority
+    node find_by_value(int v);//v-value
     void del(int v);
     void modify_key(int v,int p);
-    void heapify();
 };
 
 #endif
