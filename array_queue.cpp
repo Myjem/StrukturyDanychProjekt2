5138c75cbbbefc585347debe5cf16fd8d66c20ad
Myjem <siepkawiktor240@gmail.com>
2024-05-04 21:31:39 +0200

    ArrayQueue implementation
    i hope it works

---
+++ array_queue.cpp
@@ -0,0 +1,75 @@
+#include "array_queue.h"
+#include "queue_node.h"
+
+using namespace std;
+
+array_queue::array_queue(){}
+
+void array_queue::insert(int v, int p)
+{
+    node newnode = {v , p};
+    int index = 0;
+    while(index < ArrayQueue.get_size() && ArrayQueue.get(index).priority >= p)
+    {
+        index++;
+    }
+    ArrayQueue.append(index, newnode);
+}
+node array_queue::extract_max()
+{
+    if(isEmpty())
+    {
+        cout << "kolejka jest pusta" << endl;
+        cout << "kolejka jest tego typu";
+        return {0 ,0};
+    }
+
+    node maxnode = ArrayQueue.get(0);
+    ArrayQueue.del_front();
+    return maxnode;
+}
+node array_queue::find_max()
+{
+    if(isEmpty())
+    {
+        cout << "kolejka jest pusta";
+        return {0 ,0};
+    }
+    return ArrayQueue.get(0);
+}
+
+void array_queue::modify_key(node e,int p)
+{
+    int index = ArrayQueue.search(e);
+    ArrayQueue.set(index, {e.value, p});
+}
+int array_queue::get_size()
+{
+    return ArrayQueue.get_size();
+}
+bool array_queue::isEmpty()
+{
+    if(ArrayQueue.get_size() == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void array_queue::print() 
+{
+    if (isEmpty()) 
+    {
+        cout << "Kolejka jest pusta" << endl;
+        return;
+    }
+    for(int i = 0; i < ArrayQueue.get_size(); i++)
+    {
+        cout << "Wartość: " << ArrayQueue.get(i).value << " Priorytet: " << ArrayQueue.get(i).priority << endl;
+    }
+}
+
+array_queue::~array_queue() {}
--- array_queue.h
+++ array_queue.h
@@ -1,5 +1,22 @@
 #ifndef SD_PROJ2_ARRAY_QUEUE_H
 #define SD_PROJ2_ARRAY_QUEUE_H
 #include"base.h"
+#include"array_list.h"
 #include"queue_node.h"
+
+class array_queue:public Base{
+    private:
+    ArrayList<node> ArrayQueue;
+    public:
+    array_queue();
+    void insert(int v,int p);//v-value, p-priority
+    node extract_max();
+    node find_max();
+    void modify_key(node e,int p);
+    int get_size();
+    bool isEmpty();
+    void print();
+    ~array_queue();
+
+};
 #endif
